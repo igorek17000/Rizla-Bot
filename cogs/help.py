f3aeffae0f5303b0ca29002289e1131e1c0e011e
Simone <stranierisimone24@gmail.com>
2022-06-03 18:10:13 +0200

    Add the full code

---
+++ cogs/help.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+class menu(commands.Cog):
+ def __init__(self, bot):
+  self.bot = bot
+ @commands.command()
+ async def help(self, ctx):
+  await ctx.message.delete()
+  embed = discord.Embed(title='Rizla Commands', color=discord.Colour.from_rgb(58, 194, 243))
+  embed.add_field(name='-keystore', value='Display keystore commands information.', inline=True)
+  embed.add_field(name='-register', value='Display register commands information.', inline=True)
+  embed.add_field(name='-me', value='Display information about yourself.', inline=True)
+  embed.add_field(name='-sync', value='Sync your nation information.', inline=True)
+  embed.add_field(name='-fastbeige <resistance>', value='Display the best way for beige a enemy.', inline=True)
+  embed.add_field(name='-remind <nation_id>', value='Set a reminder for a beige target.', inline=True)
+  embed.add_field(name='-fastbeige <war_id>', value='Calculate the best attacks for beige a opponent.', inline=True)
+  embed.add_field(name='-loot', value='Display loot commands information.', inline=True)
+  embed.add_field(name='-raid', value='Display raid commands information.', inline=True)
+  await ctx.send(embed=embed)
+ @help.error
+ async def help_error(self, ctx, error):
+  if isinstance(error, commands.CommandError):
+   await ctx.send(f'{error}')
+def setup(bot):
+ bot.add_cog(menu(bot))
